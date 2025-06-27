4e989ba769e44fe691d7bbc9d369bdfb25329fc3
y.yan <y.yan98@outlook.com>
2025-06-27 11:19:36 +0800

    Debug

--- cryodata/dataset_resample.py
+++ cryodata/dataset_resample.py
@@ -2,6 +2,7 @@ from torch.utils.data.sampler import Sampler
 import random
 import numpy as np
 
+
 class MyResampleSampler(Sampler):
     def __init__(self, data, id_index_dict_pos, id_index_dict_mid, id_index_dict_neg, resample_num_pos,
                  resample_num_neg, resample_num_mid,
@@ -54,7 +55,7 @@ class MyResampleSampler(Sampler):
                                                             dataset_id_map=self.dataset_id_map,
                                                             only_mixup_bad_particles=self.only_mixup_bad_particles,
                                                             balance_per_interval=self.balance_per_interval,
-                                                            index_list=self.balance_per_interval,
+                                                            index_list=self.index_list,
                                                             # , error_balance=self.error_balance,
                                                             # mean_error_dis_dict=self.mean_error_dis_dict
                                                             # , data_error_dis_dict=self.data_error_dis_dict
@@ -91,7 +92,6 @@ class MyResampleSampler_pretrain(Sampler):
                                                    id_scores_dict=id_scores_dict,
                                                    scores_bar=scores_bar
                                                    )
-
 
     # @profile(precision=4)
     def __iter__(self):
@@ -138,7 +138,7 @@ def resample_from_id_index_dict(id_index_dict, max_number_per_sample=None, batch
                                 mean_error_dis_dict=None, data_error_dis_dict=None,
                                 id_scores_dict=None, scores_bar=0.0,
                                 balance_per_interval=False,
-interval_list=[0.3]
+                                interval_list=[0.3]
                                 ):
     random.seed(my_seed)
     resampled_index_list = []
@@ -146,7 +146,6 @@ interval_list=[0.3]
     ids_list = list(id_index_dict.keys())
     mix_up_list = []
     ids_list_all = list(id_index_dict.keys())
-
 
     if dataset_id_map is not None:
         id_map = dataset_id_map['id_map']
@@ -180,12 +179,16 @@ interval_list=[0.3]
                                                                                max_number_per_sample_pos, shuffle_type,
                                                                                shuffle_mix_up_ratio,
                                                                                is_bad_class=my_id in bad_id_list,
-                                                                               error_distribution=data_error_dis_dict[my_id] if data_error_dis_dict is not None else None,
+                                                                               error_distribution=data_error_dis_dict[
+                                                                                   my_id] if data_error_dis_dict is not None else None,
                                                                                interval_list=interval_list)
 
                 selected_index_list2, mix_up_list_added2 = get_index_per_class(id_index_dict[id_map[my_id]],
                                                                                max_number_per_sample_neg, shuffle_type,
-                                                                               shuffle_mix_up_ratio, error_distribution=data_error_dis_dict[id_map[my_id]] if data_error_dis_dict is not None else None,
+                                                                               shuffle_mix_up_ratio,
+                                                                               error_distribution=data_error_dis_dict[
+                                                                                   id_map[
+                                                                                       my_id]] if data_error_dis_dict is not None else None,
                                                                                interval_list=interval_list)
                 new_selected_index_list = selected_index_list1 + selected_index_list2
                 # random.shuffle(new_selected_index_list)
@@ -202,7 +205,9 @@ interval_list=[0.3]
                                                                                  max_number_per_sample_i, shuffle_type,
                                                                                  shuffle_mix_up_ratio,
                                                                                  is_bad_class=my_id in bad_id_list
-                                                                                 , error_distribution=data_error_dis_dict[my_id] if data_error_dis_dict is not None else None,
+                                                                                 ,
+                                                                                 error_distribution=data_error_dis_dict[
+                                                                                     my_id] if data_error_dis_dict is not None else None,
                                                                                  interval_list=interval_list)
             if only_mixup_bad_particles:
                 if my_id in bad_id_list:
@@ -225,14 +230,15 @@ interval_list=[0.3]
                 if error_balance['data_mean_error']:
                     max_number_per_sample_i = int(max_number_per_sample_i * mean_error_dis_dict[my_id])
 
-
             new_selected_index_list, mix_up_list_added = get_index_per_class(id_index_dict[my_id],
                                                                              max_number_per_sample_i, shuffle_type,
                                                                              shuffle_mix_up_ratio,
                                                                              is_bad_class=my_id in bad_id_list_all,
-                                                                             error_distribution=data_error_dis_dict[my_id] if data_error_dis_dict is not None else None,
+                                                                             error_distribution=data_error_dis_dict[
+                                                                                 my_id] if data_error_dis_dict is not None else None,
                                                                              # dataset_scores=np.array(id_scores_dict)[id_index_dict[my_id]] if id_scores_dict is not None else None,
-                                                                             dataset_scores=id_scores_dict[my_id] if id_scores_dict is not None else None,
+                                                                             dataset_scores=id_scores_dict[
+                                                                                 my_id] if id_scores_dict is not None else None,
                                                                              # bad_particles_ratio=bad_particles_ratio
                                                                              scores_bar=scores_bar,
                                                                              balance_per_interval=balance_per_interval,
@@ -298,17 +304,19 @@ def resample_from_id_index_dict_old(id_index_dict, max_number_per_sample, bad_pa
 
 
 def get_index_per_class(index_list, max_number_per_sample=None, shuffle_type=None, shuffle_mix_up_ratio=0.2,
-                        is_bad_class=False, error_distribution=None,dataset_scores=None,scores_bar=0.8,balance_per_interval=False,interval_list=[0.3]):
+                        is_bad_class=False, error_distribution=None, dataset_scores=None, scores_bar=0.8,
+                        balance_per_interval=False, interval_list=[0.3]):
     # index_list = id_index_dict[my_id]
     if balance_per_interval:
-        index_list = balance_from_scores_interval(20, index_list['score'],index_list['id'],  num_min_per_interval=128,interval_list=interval_list)
+        index_list = balance_from_scores_interval(20, index_list['score'], index_list['id'], num_min_per_interval=128,
+                                                  interval_list=interval_list)
     else:
         if isinstance(index_list, dict):
             index_list = index_list['id']
 
-    if dataset_scores is not None and scores_bar>0 and max(dataset_scores) != min(dataset_scores):
-        index_list_good=np.array(index_list)[np.array(dataset_scores) >= scores_bar].tolist()
-        index_list_bad=np.array(index_list)[np.array(dataset_scores) < scores_bar].tolist()
+    if dataset_scores is not None and scores_bar > 0 and max(dataset_scores) != min(dataset_scores):
+        index_list_good = np.array(index_list)[np.array(dataset_scores) >= scores_bar].tolist()
+        index_list_bad = np.array(index_list)[np.array(dataset_scores) < scores_bar].tolist()
         index_list = index_list_good
 
     else:
@@ -331,8 +339,10 @@ def get_index_per_class(index_list, max_number_per_sample=None, shuffle_type=Non
                 if len(index_list_bad) < int(len(selected_index_list) * shuffle_mix_up_ratio):
                     mix_up_list_added = index_list_bad
                 else:
-                    mix_up_list_added =random.sample(index_list_bad, int(len(selected_index_list) * shuffle_mix_up_ratio))
-                new_selected_index_list = selected_index_list[:max_number_per_sample - int(len(selected_index_list) * shuffle_mix_up_ratio)]
+                    mix_up_list_added = random.sample(index_list_bad,
+                                                      int(len(selected_index_list) * shuffle_mix_up_ratio))
+                new_selected_index_list = selected_index_list[
+                                          :max_number_per_sample - int(len(selected_index_list) * shuffle_mix_up_ratio)]
             else:
                 new_selected_index_list = selected_index_list[
                                           :max_number_per_sample - int(len(selected_index_list) * shuffle_mix_up_ratio)]
@@ -348,7 +358,8 @@ def get_index_per_class(index_list, max_number_per_sample=None, shuffle_type=Non
                 else:
 
                     mix_up_list_added = random.sample(index_list_bad, int(len(index_list_bad) * shuffle_mix_up_ratio))
-                new_selected_index_list = selected_index_list[:max_number_per_sample - int(len(index_list_bad) * shuffle_mix_up_ratio)]
+                new_selected_index_list = selected_index_list[
+                                          :max_number_per_sample - int(len(index_list_bad) * shuffle_mix_up_ratio)]
             else:
                 new_selected_index_list = selected_index_list
                 mix_up_list_added = []
@@ -385,7 +396,7 @@ def resample_from_id_index_dict_finetune(id_index_dict_pos, id_index_dict_mid, i
                                                            batch_size_all, shuffle_type,
                                                            shuffle_mix_up_ratio, my_seed, dataset_id_map=dataset_id_map,
                                                            only_mixup_bad_particles=only_mixup_bad_particles,
-                                                           balance_per_interval= balance_per_interval,
+                                                           balance_per_interval=balance_per_interval,
                                                            interval_list=index_list
                                                            # error_balance=error_balance,
                                                            # mean_error_dis_dict=mean_error_dis_dict,
@@ -399,7 +410,7 @@ def resample_from_id_index_dict_finetune(id_index_dict_pos, id_index_dict_mid, i
             resample_from_id_index_dict(id_index_dict_pos, resample_num_p, batch_size_all, shuffle_type,
                                         shuffle_mix_up_ratio, my_seed,
                                         only_mixup_bad_particles=only_mixup_bad_particles,
-                                        balance_per_interval= balance_per_interval,
+                                        balance_per_interval=balance_per_interval,
                                         interval_list=index_list
                                         # error_balance=error_balance,
                                         # mean_error_dis_dict=mean_error_dis_dict,
@@ -409,7 +420,7 @@ def resample_from_id_index_dict_finetune(id_index_dict_pos, id_index_dict_mid, i
             resample_from_id_index_dict(id_index_dict_neg, resample_num_n, batch_size_all, shuffle_type,
                                         shuffle_mix_up_ratio, my_seed,
                                         only_mixup_bad_particles=only_mixup_bad_particles,
-                                        balance_per_interval= balance_per_interval,
+                                        balance_per_interval=balance_per_interval,
                                         interval_list=index_list
                                         # error_balance=error_balance,
                                         # mean_error_dis_dict=mean_error_dis_dict,
@@ -420,7 +431,7 @@ def resample_from_id_index_dict_finetune(id_index_dict_pos, id_index_dict_mid, i
                 resample_from_id_index_dict(id_index_dict_mid, resample_num_mid, batch_size_all, shuffle_type,
                                             shuffle_mix_up_ratio, my_seed,
                                             only_mixup_bad_particles=only_mixup_bad_particles,
-                                            balance_per_interval= balance_per_interval,
+                                            balance_per_interval=balance_per_interval,
                                             interval_list=index_list
                                             # error_balance=error_balance, mean_error_dis_dict=mean_error_dis_dict,
                                             # data_error_dis_dict=data_error_dis_dict['mid']
@@ -429,7 +440,8 @@ def resample_from_id_index_dict_finetune(id_index_dict_pos, id_index_dict_mid, i
             random.shuffle(resampled_index_list)
     return resampled_index_list
 
-def balance_from_scores_interval(interval_num,scores,ids,num_min_per_interval=0,interval_list=[0.3]):
+
+def balance_from_scores_interval(interval_num, scores, ids, num_min_per_interval=0, interval_list=[0.3]):
     """
     Balance the dataset based on scores intervals.
     :param interval_num: Number of intervals to divide the scores into.
@@ -438,6 +450,7 @@ def balance_from_scores_interval(interval_num,scores,ids,num_min_per_interval=0,
     :param num_min_per_interval: Minimum number of samples per interval.
     :return: Balanced ids and scores.
     """
+
     if len(scores) == 0:
         return [], []
 
@@ -445,9 +458,11 @@ def balance_from_scores_interval(interval_num,scores,ids,num_min_per_interval=0,
     max_score = max(scores)
     if min_score == max_score:
         return ids, scores  # No variation in scores, return original lists
+    if len(interval_list)>1:
+        interval_num = len(interval_list)
     interval_size = (max_score - min_score) / interval_num
-    min_interval_len=len(scores)
-    scores_np= np.array(scores)
+    min_interval_len = len(scores)
+    scores_np = np.array(scores)
     ids_np = np.array(ids)
 
     intervals = [[] for _ in range(interval_num)]
@@ -460,22 +475,22 @@ def balance_from_scores_interval(interval_num,scores,ids,num_min_per_interval=0,
         if len(interval_ids) < min_interval_len:
             min_interval_len = len(interval_ids)
 
-    if min_interval_len> num_min_per_interval:
+    if min_interval_len > num_min_per_interval:
         num_min_per_interval = min_interval_len
     balanced_ids = []
     # balanced_scores = []
     if interval_list is not None and len(interval_list) > 0:
-        if len(interval_list)==1:
-            new_intervals = [1-interval_list[0]+interval_list[0]/interval_num*i for i in range(interval_num)]
+        if len(interval_list) == 1:
+            new_intervals = [1 - interval_list[0] + interval_list[0] / interval_num * i for i in range(interval_num)]
         else:
-            new_intervals =interval_list
+            new_intervals = interval_list
     else:
         new_intervals = [1 for _ in range(interval_num)]
-    for i,interval in enumerate(intervals):
-        if len(interval) < int(num_min_per_interval* new_intervals[i]):
+    for i, interval in enumerate(intervals):
+        if len(interval) < int(num_min_per_interval * new_intervals[i]):
             selected_ids = interval
         else:
-            selected_ids = random.sample(interval, int(num_min_per_interval* new_intervals[i]))
+            selected_ids = random.sample(interval, int(num_min_per_interval * new_intervals[i]))
         balanced_ids.extend(selected_ids)
 
     balanced_ids = sorted(balanced_ids)  # Sort the final list of ids
